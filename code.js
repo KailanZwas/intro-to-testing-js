b246925f5fac43aea1d5a49a52a3a90132131cd3
Kailan Zwaschka <kailan.zwaschka@gmail.com>
2022-12-01 09:04:42 -0600

    lesson 3&4

--- code.js
+++ code.js
@@ -6,8 +6,16 @@
 const helloWorld = function() {
     return "Hello, World!";
 }
-// let input = "Jane"
-function sayHello() {
 
+function sayHello(input) {
+    if (input === "Alex"){
+    return "Hello, Alex!";
+    } else if (input === "Pat"){
+        return "Hello, Pat!";
+    }
+    else return "Hello, Jane!";
 }
+
+///push
+
 
--- tests.js
+++ tests.js
@@ -18,4 +18,17 @@ describe('sayHello', function() {
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
     });
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe('string');
+    });
+    it('should return string "Hello, Jane!" when executed', function () {
+        expect(sayHello("Jane!")).toBe("Hello, Jane!");
+    });
+    it('should return string "Hello, Alex!" when executed', function () {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should return string "Hello, Pat!" ', function () {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+    it()
 });
